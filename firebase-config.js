3e89c5318b27bb3db279aa0c3a7c5de076e7cb5b
harma <harma@user.com>
2026-03-16 23:59:50 +0300

    Include firebase-config.js for Vercel deployment

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// 🔥 Firebase Configuration — ئەم فایلە زیاد بکە بۆ .gitignore
+const firebaseConfig = {
+    apiKey: "AIzaSyDZCSjDzJ27tPqGWne_zpbNuSX9M_rpsLQ",
+    authDomain: "votekrd.firebaseapp.com",
+    projectId: "votekrd",
+    storageBucket: "votekrd.firebasestorage.app",
+    messagingSenderId: "948514750647",
+    appId: "1:948514750647:web:5ad94539c3c7de38c24d32",
+    measurementId: "G-RERL6DX7W2"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
